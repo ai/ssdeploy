fbffb9e767cf1b80d16dbc0b79a426bc36498740
Andrey Sitnik <andrey@sitnik.ru>
2019-12-30 15:42:47 -0500

    Add sign command

--- sign.js
+++ sign.js
@@ -1,3 +1,17 @@
-export default async function sign () {
+import { promises as fs } from 'fs'
+import { promisify } from 'util'
+import child from 'child_process'
 
+let exec = promisify(child.exec)
+
+export default async function sign (path) {
+  let content = await fs.readFile(path)
+  let cleared = content
+    .toString()
+    .replace(/-----BEGIN PGP SIGNED MESSAGE-----\nHash:[^\n]+\n\n/, '')
+    .replace(/-----BEGIN PGP SIGNATURE-----\n\n[\W\w]+$/m, '')
+  await fs.writeFile(path, cleared)
+  await exec(`gpg --digest-algo SHA512 --clearsign ${ path }`)
+  await fs.unlink(path)
+  await fs.rename(path + '.asc', path)
 }
