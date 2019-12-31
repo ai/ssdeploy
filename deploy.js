44cae230d3eafd0b1a53b7eef6b3b4315a46e7e1
Andrey Sitnik <andrey@sitnik.ru>
2019-12-31 12:48:26 -0500

    Fix first deploy

--- deploy.js
+++ deploy.js
@@ -47,10 +47,13 @@ async function push (image) {
 
 async function run (image, project, app, region = 'us-east1') {
   await wrap('Starting image on Google Cloud Run', async () => {
-    await exec('gcloud components install beta --quiet')
+    try {
+      await exec('gcloud components install beta --quiet')
+    } catch { }
     await exec(
-      `gcloud beta run deploy ${ app } --image ${ image } ` +
-      `--project ${ project } --region=${ region } --platform managed`
+      `gcloud beta run deploy ${ app } --image ${ image }` +
+      `--project ${ project } --region=${ region } ` +
+      '--platform managed  --allow-unauthenticated'
     )
   })
 }
