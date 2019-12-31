1714c70e0e68d6d98f45e8e0c953d1471b85a2ac
Andrey Sitnik <andrey@sitnik.ru>
2019-12-30 23:21:58 -0500

    Allow to start project without CLoudflare

--- purge.js
+++ purge.js
@@ -1,16 +1,17 @@
 import callCloudflare from './call-cloudflare.js'
-import showError from './show-error.js'
+import { showWarning } from './show-error.js'
 import { wrap } from './show-spinner.js'
 
 export default async function purge () {
   if (!process.env.CLOUDFLARE_ZONE || !process.env.CLOUDFLARE_TOKEN) {
-    throw showError(
+    showWarning(
       'Get zone ID and API token in CLoudflare dashboard',
       'and set `CLOUDFLARE_TOKEN` and `CLOUDFLARE_ZONE`',
       'environment variables at your CI'
     )
+  } else {
+    await wrap('Cleaning CDN cache', async () => {
+      await callCloudflare('purge_cache', { purge_everything: true })
+    })
   }
-  await wrap('Cleaning CDN cache', async () => {
-    await callCloudflare('purge_cache', { purge_everything: true })
-  })
 }
--- show-error.js
+++ show-error.js
@@ -1,10 +1,14 @@
 import chalk from 'chalk'
 
-export default function showError (...lines) {
+export function showWarning (...lines) {
   lines = lines.map(line => {
     return line.replace(/`[^`]+`/g, i => chalk.yellow(i.slice(1, -1)))
   })
   process.stderr.write(chalk.red(lines.join('\n')) + '\n')
+}
+
+export default function showError (...lines) {
+  showWarning(...lines)
   let err = new Error('show error')
   err.own = true
   return err
